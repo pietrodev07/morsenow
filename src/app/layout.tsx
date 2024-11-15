3d2ba7698ac855c9b782f9186dc98884d6bf2573
pietrodev07 <pietro.dev.07@gmail.com>
2024-11-15 19:00:44 +0100

    feat(components): add theme-provider component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import { Metadata } from "next";
 import { PropsWithChildren } from "react";
 import { Bricolage_Grotesque } from "next/font/google";
+import { Providers } from "@/components/theme-provider";
 
 const font = Bricolage_Grotesque({ subsets: ["latin"] });
 
@@ -12,8 +13,10 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
-    <html lang="en" className={font.className}>
-      <body>{children}</body>
+    <html lang="en" className={font.className} suppressHydrationWarning>
+      <body>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
