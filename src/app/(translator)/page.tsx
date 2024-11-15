d4659b1412687b734742524f2cbfefa8f1b2aec2
pietrodev07 <pietro.dev.07@gmail.com>
2024-11-15 19:17:37 +0100

    feat(translator-page): add app-header-card component

---
+++ src/app/(translator)/page.tsx
@@ -0,0 +1,9 @@
+import { AppHeaderCard } from "./_components/app-header-card";
+
+export default function Home() {
+  return (
+    <main className="mx-auto flex max-w-6xl flex-col gap-8 p-8">
+      <AppHeaderCard />
+    </main>
+  );
+}
