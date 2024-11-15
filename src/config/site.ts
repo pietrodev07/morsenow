70d58e431d9fbf979c1299b212c32e8eccc23738
pietrodev07 <pietro.dev.07@gmail.com>
2024-11-15 19:21:07 +0100

    feat(metadata): add site-config for metadata

---
+++ src/config/site.ts
@@ -0,0 +1,8 @@
+export const siteConfig = {
+  name: "Morsenow",
+  applicationName: "Morsenow",
+  metadataBase: new URL("https://morsenow.vercel.app"),
+  robots: { index: true, follow: true, googleBot: "index, follow" },
+  keywords: ["morse", "translator", "text", "morse code", "morsenow"],
+  description: "Morsenow, a simple app to translate text to morse code",
+};
